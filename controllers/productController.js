79d2a5ef632e1781eb11f95b2dd86a3e33095830
Sabeeh Ajibade <detunjinov@gmail.com>
2022-05-04 23:43:41 +0100

    Done with users and products

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,8 @@
 const Product = require('../model/Product')
 const upload = require('../config/upload')
 
+
+//CREATE PRODUCT
 exports.createProduct = async (req, res, next) => {
     const ImageFile = upload.single('image')
     ImageFile(req, res, async(err) => {
@@ -17,7 +19,7 @@ exports.createProduct = async (req, res, next) => {
                 category : req.body.category,
             }
             const savedProduct = await Product.create(data)
-            res.status('201').json({data : savedProduct})
+            return res.status('201').json({data : savedProduct})
         } catch (error) {
             console.log(error)
             return next(res.status(403).json(error))
@@ -25,6 +27,7 @@ exports.createProduct = async (req, res, next) => {
     })
 }
 
+//GET ALL PRODUCTS
 exports.getAllProducts = async (req, res, next) => {
     try {
         const allProducts = await Product.find()
@@ -34,6 +37,7 @@ exports.getAllProducts = async (req, res, next) => {
     }
 }
 
+//GET SINGLE PRODUCT
 exports.getSingleProduct = async (req, res, next) => {
     try {
         const singleProduct = await Product.findById(req.params.id)
@@ -43,6 +47,32 @@ exports.getSingleProduct = async (req, res, next) => {
     }
 }
 
+//UPDATE PRODUCT
+exports.updateProduct = async (req, res, next) => {
+    const ImageFile = upload.single('image')
+    ImageFile(req, res, async(err) => {
+        try {
+            const { file } = req
+            if(err){
+                throw err
+            }
+            updateData = {
+                image : `uploads/products/${file.filename}`,
+                description : req.body.description,
+                name : req.body.name,
+                price : req.body.price,
+                category : req.body.category,
+            }
+       const updatedProduct = await Product.findByIdAndUpdate(req.params.id, updateData, {new : true})
+            res.status(200).json({data : updatedProduct})
+        } catch (error) {
+            console.log(error)
+            return next(res.status(500).json(error))
+        }
+    })
+}
+
+//DELETE PRODUCT
 exports.deleteProduct = async (req, res, next) => {
     try {
         const deleted = await Product.findByIdAndDelete(req.params.id)
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { createProduct, getAllProducts, getSingleProduct, deleteProduct } = require('../controllers/productController')
+const { createProduct, getAllProducts, getSingleProduct, deleteProduct, updateProduct } = require('../controllers/productController')
 const verifyToken = require('../middlewares/verifyToken')
 const { verifyAdmin } = require('../middlewares/verifyAdmin')
 
@@ -10,5 +10,6 @@ router.use(verifyToken)
 
 router.post('/create', verifyAdmin(false), createProduct)
 router.delete('/delete/:id', verifyAdmin(false), deleteProduct)
+router.put('/update/:id', verifyAdmin(false), updateProduct)
 
 module.exports = router
